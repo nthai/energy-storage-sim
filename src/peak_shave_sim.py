fe3b391d537954dab11bfe4397febbc2429fa694
nthai <nthaitrx@gmail.com>
2022-07-07 15:17:36 +0200

    moved pandas dataframe into sim class

--- src/peak_shave_sim.py
+++ src/peak_shave_sim.py
@@ -8,7 +8,7 @@ class PeakShaveEnv(gym.Env):
     def __init__(self, config) -> None:
         super().__init__()
 
-        self.df = self._read_df(config['filename'])
+        # self.df = self._read_df(config['filename'])
         self.limdelta = config['delta_limit']
         self.upperlim = config['Plim_upper']
         self.lowerlim = config['Plim_lower']
@@ -21,16 +21,16 @@ class PeakShaveEnv(gym.Env):
         self.reset()
 
     def reset(self):
-        self.timestep = 0
-        self.maxtime = len(self.df)
+        # self.timestep = 0
+        # self.maxtime = len(self.df)
         self.ehub.reset()
 
-    def _read_df(self, fname: str) -> pd.DataFrame:
-        df = pd.read_csv(fname)
-        df['timestamp'] = pd.to_datetime(df['timestamp'],
-                                         format='%m%d%Y %H:%M')
-        df['net'] = df['Load (kWh)'] - df['PV (kWh)']
-        return df
+    # def _read_df(self, fname: str) -> pd.DataFrame:
+    #     df = pd.read_csv(fname)
+    #     df['timestamp'] = pd.to_datetime(df['timestamp'],
+    #                                      format='%m%d%Y %H:%M')
+    #     df['net'] = df['Load (kWh)'] - df['PV (kWh)']
+    #     return df
 
     def get_available_actions(self):
         '''Possible actions:
@@ -52,65 +52,87 @@ class PeakShaveEnv(gym.Env):
 
         return availables
 
-    def step(self, action):
+    def step(self, action, pnet, price):
         state = None
         reward = None
+        done = False
         infos = []
+        report = ''
 
-        # pnet is the power demand. self.df.iat[self.timestep, 4] is the same as
-        # self.df.iloc[self.timestep]['net']
-        tdelta = 1
-        pnet = self.df.iat[self.timestep, 4] / tdelta
-        report = f'Net demand: {pnet:8.2f} kW, '
-        infos += [pnet]
+        if __debug__:
+            report += f'Demand: {pnet:8.2f} kW, '
+
+        pbought = 0
         if pnet > self.upperlim:
             # the demand is higher than the upper limit, take energy from the battery
             pdischarge = pnet - self.upperlim
             total_discharge, total_selfdischarge = self.ehub.discharge(pdischarge)
             pbought = pnet - total_discharge
-            report += (f'Purchased: {pbought:8.2f} kW, ')
-            report += (f'Discharged: {total_discharge:6.2f} kW, ' +
-                       f'Self-discharged: {total_selfdischarge:6.2f} kW ')
+
+            if __debug__:
+                report += (f'Purchased: {pbought:8.2f} kW, ')
+                report += (f'Discharged: {total_discharge:6.2f} kW, ' +
+                           f'SelfD: {total_selfdischarge:6.2f} kW ')
         elif pnet < self.lowerlim:
             # the demand is lower than the lower limit, use the excess to charge
             # the battery
             pcharge = self.lowerlim - pnet
             total_charge, total_selfdischarge = self.ehub.charge(pcharge)
             pbought = pnet + total_charge
-            report += (f'Purchased: {pbought:8.2f} kW, ')
-            report += (f'Charged:    {total_charge:6.2f} kW, ' +
-                       f'Self-discharged: {total_selfdischarge:6.2f} kW ')
+
+            if __debug__:
+                report += (f'Purchased: {pbought:8.2f} kW, ')
+                report += (f'Charged:    {total_charge:6.2f} kW, ' +
+                           f'SelfD: {total_selfdischarge:6.2f} kW ')
         else:
+            # we do nothing, but there is still self-discharge!
             _, _, total_selfdischarge = self.ehub.do_nothing()
             pbought = pnet
-            report += (f'Purchased: {pbought:8.2f} kW, ')
-            report += (f'No charge/discharge,   ' +
-                       f'Self-discharged: {total_selfdischarge:6.2f} kW ')
-            
-        maxsoc = self.ehub.get_maxsoc()
-        soc = self.ehub.get_soc()
-        report += f'State-of-Charge: {soc:8.2f} kWh ({soc/maxsoc*100:.2f}%)'
-        print(report)
 
-        self.timestep += 1
+            if __debug__:
+                report += (f'Purchased: {pbought:8.2f} kW, ')
+                report += (f'No charge/discharge,   ' +
+                           f'SelfD: {total_selfdischarge:6.2f} kW ')
 
-        done = (self.timestep >= self.maxtime)
+        cost = price * pbought / 100
+        reward = -cost
+
+        if __debug__:
+            maxsoc = self.ehub.get_maxsoc()
+            soc = self.ehub.get_soc()
+            report += f'SOC: {soc:8.2f} kWh ({soc/maxsoc*100:6.2f}%) '
+            report += f'Price: {price:7.2f} '
+            report += f'Money spent: {cost:7.2f}'
+            print(report)
+
         return state, reward, done, infos
 
 class PeakShaveSim:
     def __init__(self, config):
         self.env = PeakShaveEnv(config)
+        self.df = self._read_df(config['filename'])
+        
+    def _read_df(self, fname: str) -> pd.DataFrame:
+        df = pd.read_csv(fname)
+        df['timestamp'] = pd.to_datetime(df['timestamp'],
+                                         format='%m%d%Y %H:%M')
+        df['net'] = df['Load (kWh)'] - df['PV (kWh)']
+        return df
 
     def run(self):
-        cnt = 0
-        while not (obs := self.env.step(0))[2]:
-            pass
+        total_costs = 0
+        for _, datarow in self.df.iterrows():
+            pnet = datarow['net']
+            price = datarow['price (cents/kWh)']
+            _, reward, _, _ = self.env.step(0, pnet, price)
+            total_costs += -reward
+        print(f'Total Cost: {total_costs}')
 
 def main():
     config = {
         'filename': FILENAME,
-        'Plim_upper': 2000,
-        'Plim_lower': 1500,
+        'Plim_upper': 2500,
+        'Plim_lower': 1000,
         'delta_limit': 1,
         'LiIonBattery': 3,
         'Flywheel': 3,
